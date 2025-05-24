39d8b5652393beba399e87cecb78623ac9b03d16
dkerishna <dineshkerishna@gmail.com>
2025-05-24 13:31:30 +0800

    tickbox

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const API_URL = 'https://jsonplaceholder.typicode.com/todos';
+const apiURL = 'https://jsonplaceholder.typicode.com/todos';
 const userSelect = document.getElementById('userSelect');
 const todoList = document.getElementById('todoList');
 let allTodos = [];
 
 async function fetchTodos() {
   try {
-    const response = await fetch(API_URL);
+    const response = await fetch(apiURL);
     const todos = await response.json();
     allTodos = todos;
 
@@ -31,7 +31,16 @@ function displayTodos(todos) {
   todoList.innerHTML = '';
   todos.forEach(todo => {
     const li = document.createElement('li');
-    li.textContent = `${todo.userId}: ${todo.title}`;
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = todo.completed;
+
+    const span = document.createElement('span');
+    span.textContent = `${todo.userId}: ${todo.title}`;
+
+    li.appendChild(checkbox);
+    li.appendChild(span);
     todoList.appendChild(li);
   });
 }
